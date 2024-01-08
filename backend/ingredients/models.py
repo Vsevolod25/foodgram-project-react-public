0edddcf43e85906f94df96dd9b7417c5e3668ecc
Vsevolod Kolupatin <vsevolod25@yandex.ru>
2024-01-08 20:23:25 +0400

    Modify ingredient model constraints

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -3,11 +3,19 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(
-        max_length=200, unique=True , verbose_name='название'
+        max_length=200, verbose_name='название'
     )
     measurement_unit = models.CharField(
         max_length=200, verbose_name='единицы измерения'
     )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                name='unique_ingredient',
+                fields=('name', 'measurement_unit')
+            ),
+        )
 
     def __str__(self):
         return self.name
