06dbf15149a347e622e574c3fb8cee3c4f446071
Vsevolod Kolupatin <vsevolod25@yandex.ru>
2024-01-17 18:01:19 +0400

    Add favorited_num to recipe admin model, debug to false, docker-compose to images

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,7 +5,15 @@ from .models import Recipe
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'text', 'author', 'pub_date', 'cooking_time',)
+    list_display = (
+        'name', 'text', 'author', 'pub_date', 'cooking_time', 'favorited_num',
+    )
+    readonly_fields = ('favorited_num',)
     search_fields = ('name', 'author',)
     list_filter = ('name', 'author', 'pub_date', 'ingredients', 'tags',)
     empty_value_display = '-пусто-'
+
+    def favorited_num(self, obj):
+        return obj.favorited_num()
+
+    favorited_num.short_description = 'Добавлено в избранное'
