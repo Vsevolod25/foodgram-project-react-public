b88412765d5659447888f5e5237708f2e4f9e539
Vsevolod Kolupatin <vsevolod25@yandex.ru>
2024-01-17 20:24:23 +0400

    Add favorite and shoppingcart models to admin, debug off

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import (
+    Favorite, Recipe, RecipeIngredient, RecipeTag, ShoppingCart
+)
 
 
 @admin.register(Recipe)
@@ -17,3 +19,19 @@ class RecipeAdmin(admin.ModelAdmin):
         return obj.favorited_num()
 
     favorited_num.short_description = 'Добавлено в избранное'
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('favorite',)
+    search_fields = ('favorite__name',)
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('shopping_cart',)
+    search_fields = ('shopping_cart__name',)
+
+
+admin.site.register(RecipeIngredient)
+admin.site.register(RecipeTag)
