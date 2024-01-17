a5d12d76b889f353a1d8e81047c54796efafc8e5
Vsevolod Kolupatin <vsevolod25@yandex.ru>
2024-01-17 17:38:03 +0400

    Fix image representation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -81,14 +81,6 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     def get_subscription(self, obj):
         user_id = self.context['view'].kwargs['user_id']
         return get_object_or_404(User, id=user_id)
-    
-    def decode_image(self, image):
-        image = str(image)
-        if isinstance(image, str) and image.startswith('data:image'):
-            format, imgstr = image.split(';base64,')
-            ext = format.split('/')[-1]
-            image = ContentFile(b64decode(imgstr), name='image.' + ext)
-        return image
 
     def to_representation(self, instance):
         request = self.context['request']
@@ -111,7 +103,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
                 {
                     'id': current_recipe.id,
                     'name': current_recipe.name,
-                    'image': self.decode_image(current_recipe.image),
+                    'image': str(current_recipe.image),
                     'cooking_time': current_recipe.cooking_time
                 } for current_recipe in subscription_recipes
             ],
